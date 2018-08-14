f08d19c4ad2029b59352e45a4a0234cc54850112
Fenton Martin <fentonxm@gmail.com>
2018-08-15 01:10:58 +0700

    Update View Bank

--- view/bank.php
+++ view/bank.php
@@ -2,23 +2,29 @@
 <html>
   <body>
   </br>
-    <table width="50%" align="center" border="1">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nama</th>
-          <th>Alamat</th>
-          <th>Telepon</th>
-        </tr>
-      </thead>
-      <?php for ($i=0; $i < count($_SESSION['id_bank']); $i++) {?>
-        <tr>
-          <td><?php echo $_SESSION['id_bank'][$i];?></td>
-          <td><?php echo $_SESSION['name_bank'][$i];?></td>
-          <td><?php echo $_SESSION['address_bank'][$i];?></td>
-          <td><?php echo $_SESSION['phone_bank'][$i];?></td>
-        </tr>
-      <?php } ?>
-    </table>
+    <div align=center>
+      <title>Bank Page</title>
+      <p>
+        <font size='4pt'>List Bank</font>
+      </p>
+      <table width="50%" align="center" border="1">
+        <thead>
+          <tr>
+            <th>ID Bank</th>
+            <th>Nama Bank</th>
+            <th>Alamat Bank</th>
+            <th>Telepon Bank</th>
+          </tr>
+        </thead>
+        <?php for ($i=0; $i < count($_SESSION['id_bank']); $i++) {?>
+          <tr>
+            <td><?php echo $_SESSION['id_bank'][$i];?></td>
+            <td><?php echo $_SESSION['name_bank'][$i];?></td>
+            <td><?php echo $_SESSION['address_bank'][$i];?></td>
+            <td><?php echo $_SESSION['phone_bank'][$i];?></td>
+          </tr>
+        <?php } ?>
+      </table>
+    </div>
   </body>
 </html>
