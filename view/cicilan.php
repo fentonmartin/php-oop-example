116d8b189bee2d8ad4e8a54161088a285fe0853a
Fenton Martin <fentonxm@gmail.com>
2018-08-15 01:14:24 +0700

    Update View Cicilan

--- view/cicilan.php
+++ view/cicilan.php
@@ -2,23 +2,33 @@
 <html>
   <body>
   </br>
-    <table width="50%" align="center" border="1">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Date</th>
-          <th>Jumlah</th>
-          <th>Saldo</th>
-        </tr>
-      </thead>
-      <?php for ($i=0; $i < count($_SESSION['id_cicilan']); $i++) {?>
-        <tr>
-          <td><?php echo $_SESSION['id_cicilan'][$i];?></td>
-          <td><?php echo $_SESSION['date_cicilan'][$i];?></td>
-          <td><?php echo $_SESSION['jumlah_cicilan'][$i];?></td>
-          <td><?php echo $_SESSION['saldo_cicilan'][$i];?></td>
-        </tr>
-      <?php } ?>
-    </table>
+    <div align=center>
+      <title>Cicilan Page</title>
+      <p>
+        <font size='4pt'>List Cicilan</font>
+      </p>
+      <table width="50%" align="center" border="1">
+        <thead>
+          <tr>
+            <th>ID Cicilan</th>
+            <th>Tanggal Cicilan</th>
+            <th>Jumlah Cicilan</th>
+            <th>Saldo Cicilan</th>
+          </tr>
+        </thead>
+        <?php for ($i=0; $i < count($_SESSION['id_cicilan']); $i++) {?>
+          <tr>
+            <td><?php echo $_SESSION['id_cicilan'][$i];?></td>
+            <td><?php echo $_SESSION['date_cicilan'][$i];?></td>
+            <td><?php echo $_SESSION['jumlah_cicilan'][$i];?></td>
+            <td><?php echo $_SESSION['saldo_cicilan'][$i];?></td>
+          </tr>
+        <?php } ?>
+      </table>
+      </br>  
+      <form action="http://localhost/php-oop-example/form/cicilan.php">
+        <input align="right" type="submit" value="Add new record" />
+      </form>
+    </div>
   </body>
 </html>
