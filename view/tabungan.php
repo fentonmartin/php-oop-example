1d481f20dd4656b3d8fc7ab6262cd49742052bae
Fenton Martin <fentonxm@gmail.com>
2018-08-15 01:19:09 +0700

    Update View Tabungan

--- view/tabungan.php
+++ view/tabungan.php
@@ -7,23 +7,28 @@
       <p>
         <font size='4pt'>List Tabungan</font>
       </p>
-    <table width="50%" align="center" border="1">
-      <thead>
-        <tr>
-          <th>ID Tabungan</th>
-          <th>Tanggal Tabungan</th>
-          <th>Jumlah Tabungan</th>
-          <th>Saldo Tabungan</th>
-        </tr>
-      </thead>
-      <?php for ($i=0; $i < count($_SESSION['id_tabungan']); $i++) {?>
-        <tr>
-          <td><?php echo $_SESSION['id_tabungan'][$i];?></td>
-          <td><?php echo $_SESSION['date_tabungan'][$i];?></td>
-          <td><?php echo $_SESSION['jumlah_tabungan'][$i];?></td>
-          <td><?php echo $_SESSION['saldo_tabungan'][$i];?></td>
-        </tr>
-      <?php } ?>
-    </table>
+      <table width="50%" align="center" border="1">
+        <thead>
+          <tr>
+            <th>ID Tabungan</th>
+            <th>Tanggal Tabungan</th>
+            <th>Jumlah Tabungan</th>
+            <th>Saldo Tabungan</th>
+          </tr>
+        </thead>
+        <?php for ($i=0; $i < count($_SESSION['id_tabungan']); $i++) {?>
+          <tr>
+            <td><?php echo $_SESSION['id_tabungan'][$i];?></td>
+            <td><?php echo $_SESSION['date_tabungan'][$i];?></td>
+            <td><?php echo $_SESSION['jumlah_tabungan'][$i];?></td>
+            <td><?php echo $_SESSION['saldo_tabungan'][$i];?></td>
+          </tr>
+        <?php } ?>
+      </table>
+      </br>  
+      <form action="http://localhost/php-oop-example/form/tabungan.php">
+        <input align="right" type="submit" value="Add new record" />
+      </form>
+    </div>
   </body>
 </html>
